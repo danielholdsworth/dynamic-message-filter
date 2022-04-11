cee70474e02de307dcdd35263b79c7e89cb07008
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-04-11 16:52:07 +0100

    fixed script

--- src/scripts/gitlogchecker.sh
+++ src/scripts/gitlogchecker.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
+#GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
+GIT_COMMIT_MSG="3e0060fbff8b10c673879bd7991ff477592a9adf (HEAD -> alpha, origin/alpha) testing what [api][auth]"
 
-if [[ $GIT_COMMIT_MSG == *"[$PARAM]"* ]]; then
-  echo "Continuation Requested!"
-  echo '{"'"$PARAM"'":true}' > /tmp/pipeline-parameters.json
+PARAM_FILE="/tmp/pipeline-parameters.json"
+
+PARAM="e2e
+api
+auth"
+
+while IFS= read -r line; do
+  if [[ $GIT_COMMIT_MSG == *"[$line]"* ]]; then
+    echo "Continuation Requested!"
+    echo '{"'"$line"'":true}' >> $PARAM_FILE
+  else
+    echo "No Continuation Requested!"
+  fi
+done <<< "$PARAM"
+
+filesize=$(wc -c <"$PARAM_FILE" | xargs)
+echo $filesize
+
+if [ $filesize > "3" ]; then
+  IS_TRIGGERED=true
 else
-  echo "No Continuation Requested!"
-  echo '{}' > /tmp/pipeline-parameters.json
+  IS_TRIGGERED=false
+fi
+
+echo $IS_TRIGGERED
+
+if [ IS_TRIGGERED = false ]; then
+  echo '{}' >> /tmp/pipeline-parameters.json
 fi
