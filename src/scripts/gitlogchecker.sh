b9a24af0ee804b0b58d30cee0a26314914c0738f
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-04-11 12:58:25 +0100

    replaced with git filtering orb [semver:major]

--- src/scripts/gitlogchecker.sh
+++ src/scripts/gitlogchecker.sh
@@ -4,5 +4,5 @@ GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 
 if [[ $GIT_COMMIT_MSG == *"[e2e]"* ]]; then
   echo "Continuation Requested!"
-  $PARAM = {"e2e":true}
+  #$PARAM = {"e2e":true}
 fi
